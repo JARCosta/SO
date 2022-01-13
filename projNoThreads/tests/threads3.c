c989d701f3ada8156e8384c8004f43d63e1ac247
João Costa <67013643+JCosta-PT@users.noreply.github.com>
2022-01-13 14:40:06 +0000

    finalized threads1.c

---
+++ projNoThreads/tests/threads3.c
@@ -0,0 +1,24 @@
+// criar file x
+// criar threads
+    // escreve y
+// fecha x
+
+
+
+
+
+
+/*
+open(x)
+thread 1:
+    write(y)
+thread 2:
+    write(y)
+thread 3:
+    .
+    .
+    .
+close(x)
+exit(0)
+ACCEPTED
+*/
