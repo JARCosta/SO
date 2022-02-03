91214d7fef9f74ffe2f00f378160d6e0576a2f0d
Francisco <kiko.n.sanchez@outlook.com>
2022-02-03 23:02:43 +0000

    fucking finally

--- projeto 2/client/tecnicofs_client_api.c
+++ projeto 2/client/tecnicofs_client_api.c
@@ -3,24 +3,22 @@
 
 int session_id;
 int server_pipe, client_pipe;
-char *client_pipe_name;
+char const *client_pipe_name;
 //void* message_to_server;
 
 
-
-int send_message_to_server(int op_code, void* input_struct, size_t size_of_struct){
+int send_message_to_server(int op_code, void* input, size_t size_of_input){
+    //printf("\t sending message\n");
     size_t message_size;
     void* message_to_server;
-        message_size = sizeof(char) + size_of_struct;
+        message_size = sizeof(char) + size_of_input;
         message_to_server = malloc(message_size);
         char op_char = '0' + op_code;
         memcpy(message_to_server, &op_char, sizeof(char));
-        memcpy(message_to_server + sizeof(char), input_struct, size_of_struct);
-    size_t size = write(server_pipe, message_to_server, message_size);
-//    printf("\t%d B\n",(int)size);
+        memcpy(message_to_server + sizeof(char), input, size_of_input);
+    if(-1 == write(server_pipe, message_to_server, message_size)) return -1;
     return 0;
 }
-
 
 
 int mem_set_and_cpy(void* dest,char car, size_t size, char* src){
@@ -28,21 +26,6 @@ int mem_set_and_cpy(void* dest,char car, size_t size, char* src){
     memcpy(dest, src, strlen(src));
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
@@ -58,6 +41,8 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
         }
     }
 
+    client_pipe_name = client_pipe_path;
+    
 // open server pipe
     server_pipe = open(server_pipe_path, O_WRONLY);
     if(server_pipe == -1){
@@ -66,7 +51,7 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     }
 
     mount_struct input;
-    mem_set_and_cpy(&input.client_pipe_name,'\0',sizeof(char) * CLIENT_NAME_SIZE, client_pipe_path);
+    mem_set_and_cpy(&input.client_pipe_name,'\0',sizeof(char) * CLIENT_NAME_SIZE, (char*)client_pipe_path);
 
     send_message_to_server(TFS_OP_CODE_MOUNT,&input,sizeof(mount_struct));
 
@@ -77,17 +62,11 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
         return -1;
     }
 
-    size_t red = read(client_pipe, &session_id, sizeof(int));
+    if(-1 == read(client_pipe, &session_id, sizeof(int))) return -1;
     if (session_id == -1) return -1;
     printf("\t pipe path: %s\n\t session_id: %d\n", input.client_pipe_name, session_id);
     return 0;
 }
-
-
-
-
-
-
 
 
 int tfs_unmount() {
@@ -104,42 +83,28 @@ int tfs_unmount() {
     
     //printf("\t sending %s to server\n", (char*)message_to_server);
 
-    int x = write(server_pipe, message_to_server, sizeof(char) + sizeof(unmount_struct));
-    if(x == -1){
-        printf("ERROR: writing\n");
+    if(-1 == write(server_pipe, message_to_server, sizeof(char) + sizeof(unmount_struct)) ) return -1;
+
+
+    int error = 0;
+
+    if(-1 != read(server_pipe, &error, sizeof(int))) return error;
+      
+    if (close(server_pipe) == -1){
+        printf("ERROR: Couldn't close client pipe.\n");
+        return -1;
+    }   
+    if (close(client_pipe) == -1){
+        printf("ERROR: Couldn't close client pipe.\n");
         return -1;
     }
-
-    int error;
-
-    read(server_pipe, &error, sizeof(int));
     
-    if (error == 1){    
-        if (close(server_pipe) == -1){
-            printf("ERROR: Couldn't close client pipe.\n");
-            return -1;
-        }
-        
-        if (close(client_pipe) == -1){
-            printf("ERROR: Couldn't close client pipe.\n");
-            return -1;
-        }
-        
-        if (unlink(client_pipe_name) == -1){
-            printf("ERROR: Couldn't unmount client session.\n");
-            return -1;
-        }
+    if (unlink(client_pipe_name) == -1){
+        printf("ERROR: Couldn't unmount client session.\n");
+        return -1;
     }
-
-    return 0;
+    return error;
 }
-
-
-
-
-
-
-
 
 
 int tfs_open(char const *name, int flags) {
@@ -148,21 +113,15 @@ int tfs_open(char const *name, int flags) {
     open_struct input;
     input.flag = flags;
     input.session_id = session_id;
-    mem_set_and_cpy(&input.name, '\0', CLIENT_NAME_SIZE, name);
+    mem_set_and_cpy(&input.name, '\0', CLIENT_NAME_SIZE, (char*)name);
 
     send_message_to_server(TFS_OP_CODE_OPEN,&input,sizeof(input));
     
     int buffer;
-    size_t red = read(client_pipe, &buffer, sizeof(int));
+    if(-1 == read(client_pipe, &buffer, sizeof(int))) return -1;
     printf("\t opened %d\n",buffer);
     return buffer;
 }
-
-
-
-
-
-
 
 
 int tfs_close(int fhandle) {
@@ -175,16 +134,10 @@ int tfs_close(int fhandle) {
     send_message_to_server(TFS_OP_CODE_CLOSE,&input,sizeof(input));
 
     int buffer;
-    size_t red = read(client_pipe, &buffer, sizeof(int));
+    if(-1 == read(client_pipe, &buffer, sizeof(int))) return -1;
     printf("\t closed %d\n",buffer);
     return buffer;
 }
-
-
-
-
-
-
 
 
 ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
@@ -204,16 +157,10 @@ ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
     send_message_to_server(TFS_OP_CODE_WRITE, message, message_size);
     
     ssize_t written;
-    read(client_pipe, &written, sizeof(ssize_t));
+    if(-1 == read(client_pipe, &written, sizeof(ssize_t))) return -1;
     printf("\t wrote %d B\n", (int)written);
     return written;
 }
-
-
-
-
-
-
 
 
 ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
@@ -226,17 +173,20 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
 
     send_message_to_server(TFS_OP_CODE_READ, &input, sizeof(input));
 
-    size_t red = read(client_pipe, buffer, len);
-    printf("\t read %d B, %s\n", (int)red, buffer);
+    if(-1 == read(client_pipe, buffer, len)) return -1;
+//    printf("\t read %d B, %s\n", (int)red, buffer);
 
-    return strlen(buffer);
+    return (ssize_t)strlen(buffer);
 }
 
 
-
-
-
-
 int tfs_shutdown_after_all_closed() {
+    printf("TFS_SHUTDOWN_AFTER_ALL_CLOSED:\n");
+    send_message_to_server(TFS_OP_CODE_SHUTDOWN_AFTER_ALL_CLOSED, &session_id, sizeof(int));
+    int error;
+    if(-1 == read(client_pipe, &error, sizeof(int))) return -1;
+    if (error == -1){
+        printf("\t ERROR: Couldnt call function\n");
+    }
     return -1;
 }
--- projeto 2/fs/tfs_server.c
+++ projeto 2/fs/tfs_server.c
@@ -6,46 +6,11 @@
 
 session session_list[MAX_SESSIONS];
 int free_sessions[MAX_SESSIONS] = {FREE};
-int opened_files_owners[1024] = {-1};
+//int opened_files_owners[1024] = {-1};
 int server_pipe;
+pthread_mutex_t server_thread_lock;
 
-void* thread_func(int session_id){
-    pthread_mutex_lock(&session_list[session_id].lock);
-    pthread_cond_wait(&session_list[session_id].var, &session_list[session_id].lock);
-    //pthread_mutex_unlock(&session_list[session_id].lock);
-
-    char op_char = session_list[session_id].op_code;
-
-    if (op_char == '1'){
-        
-    }
-    else if(op_char == '2'){
-
-
-    }
-    else if(op_char == '3'){
-
-
-    }
-    else if(op_char == '4'){
-
-
-    }
-    else if(op_char == '5'){
-
-
-    }
-    else if(op_char == '6'){
-
-
-    }
-    else if(op_char == '7'){
-
-
-    }
-}
-
-int thread_mount(){
+void thread_mount(){
     printf("TFS_MOUNT:\n");
     int session_id;
     char client_name[40] = {'\0'};
@@ -55,7 +20,7 @@ int thread_mount(){
     }                
     session_id = -1;
 
-    for (int i = 0; i < MAX_SESSIONS; i++){ // get first free session or wait for one, in the future, use signals
+    for (int i = 0; i < MAX_SESSIONS; ++i){ // get first free session or wait for one, in the future, use signals
         if (free_sessions[i] == FREE){
             session_list[i].client_path_name = client_name;
             session_id = i;
@@ -68,86 +33,133 @@ int thread_mount(){
     int return_pipe = open(client_name, O_WRONLY);
     if(return_pipe  == -1){
         printf("ERROR: Couldnt open client pipe for writing.\n");
-        return -1;
+        return;
     }
     if (session_id != -1) session_list[session_id].client_pipe = return_pipe;
-    int written = write(return_pipe, &session_id, sizeof(int));
-    if(written <= 0){
-        printf("ERROR: writing\n");
-    }
+    if(-1 == write(return_pipe, &session_id, sizeof(int))) return;
 }
 
-int thread_unmount(int session_id){
+void thread_unmount(int session_id){
     
     int error = 1;
     
     if (close(session_list[session_id].client_pipe) == -1){
         error = -1;
         printf("ERROR: couldnt close client pipe\n");
-        write(session_list[session_id].client_pipe, &error, sizeof(int));
-        return -1;
+        if(write(session_list[session_id].client_pipe, &error, sizeof(int)) == -1) return;
+        return;
     }
 
     free_sessions[session_id] = FREE;
-    write(session_list[session_id].client_pipe, &error, sizeof(int));
     
     printf("\t unmounted session %d\n", session_id);
+    free(session_list[session_id].buffer);
+    session_list[session_id].buffer = NULL;
 }
 
-int thread_open(){
-    printf("TFS_OPEN:\n");
+void thread_open(int session_id){
+
     open_struct input;
-    read(server_pipe, &input, sizeof(open_struct));
+    memcpy(&input, session_list[session_id].buffer, sizeof(open_struct));
+
     int buffer = tfs_open(input.name,input.flag);
-    opened_files_owners[buffer] = input.session_id;
-    write(session_list[input.session_id].client_pipe, &buffer, sizeof(buffer));
+    //opened_files_owners[buffer] = input.session_id;
+    if(write(session_list[input.session_id].client_pipe, &buffer, sizeof(buffer)) == -1) return;
+    free(session_list[session_id].buffer);
+    session_list[session_id].buffer = NULL;
 }
 
-int thread_close(){
-    printf("TFS_CLOSE:\n");
+void thread_close(int session_id){
+
     close_struct input;
-    read(server_pipe, &input, sizeof(close_struct));
-    
+    memcpy(&input, session_list[session_id].buffer, sizeof(close_struct));
+    /*
     int owner_flag;
     if(input.session_id != opened_files_owners[input.session_id]){
         owner_flag = 1;
     }
+    */
     
     int buffer = tfs_close(input.fhandle);
-    write(session_list[input.session_id].client_pipe, &buffer, sizeof(buffer));
+    if(write(session_list[session_id].client_pipe, &buffer, sizeof(buffer)) == -1) return;
     printf("\t closed %d\n",buffer);
-
+    free(session_list[session_id].buffer);
+    session_list[session_id].buffer = NULL;
 }
 
-int thread_write(){
-    printf("TFS_WRITE:\n");
+void thread_write(int session_id){
 
     write_struct input;
-    read(server_pipe, &input, sizeof(write_struct));
-    char buffer[input.len + 1];
-    read(server_pipe, &buffer, input.len);
-    
-    ssize_t written;
-    written = tfs_write(input.fhandle, &buffer, input.len);
-    size_t size = write(session_list[input.session_id].client_pipe, &written, sizeof(ssize_t));
-    printf("\t wrote %d B\n", written);
-}
-
-int thread_read(){
-    printf("TFS_READ:\n");
-    read_struct input;
-    read(server_pipe, &input, sizeof(read_struct));
-    ssize_t red;
+    memcpy(&input, session_list[session_id].buffer, sizeof(write_struct));
     char buffer[input.len];
-    red = tfs_read(input.fhandle, &buffer, input.len);
-    buffer[red] = '\0';
-    write(session_list[input.session_id].client_pipe, &buffer, input.len);
-    printf("\t read %d B\n", red);
+    memcpy(&buffer, session_list[session_id].buffer + sizeof(write_struct), input.len);
+
+    printf("\t written on server: %s\n", buffer);
+    
+    ssize_t written = tfs_write(input.fhandle, &buffer, input.len);
+    if(write(session_list[input.session_id].client_pipe, &written, sizeof(ssize_t)) == -1) return;
+    
+    printf("\t wrote %d B\n", (int)written);
+    free(session_list[session_id].buffer);
+    session_list[session_id].buffer = NULL;
 }
 
-int thread_destroy_after_all_closed(){
-    return -1;
+void thread_read(int session_id){
+
+    read_struct input;
+    memcpy(&input, session_list[session_id].buffer, sizeof(read_struct));
+    
+    char buffer[input.len];
+    ssize_t red = tfs_read(input.fhandle, &buffer, input.len);
+    buffer[red] = '\0';
+    if(write(session_list[input.session_id].client_pipe, &buffer, input.len) == -1) return;
+    printf("\t read %s( %d B)\n",buffer, (int)red);
+    free(session_list[session_id].buffer);
+    session_list[session_id].buffer = NULL;
 }
+
+void thread_destroy_after_all_closed(int session_id){
+    int error = tfs_destroy_after_all_closed();
+    if(write(session_list[session_id].client_pipe, &error, sizeof(int)) == -1) return;
+}
+
+void* thread_func(void *session_void){
+    int session_id = *(int*)session_void;
+    session_id -= 1;
+    while(1) {
+        printf(" %d\t sleeping\n", session_id);
+        pthread_mutex_lock(&session_list[session_id].lock);
+        pthread_mutex_unlock(&server_thread_lock);
+        pthread_cond_wait(&session_list[session_id].var, &session_list[session_id].lock);
+        pthread_mutex_unlock(&session_list[session_id].lock);
+        printf(" %d\t awake\n", session_id);
+
+        char op_char = session_list[session_id].op_code;
+
+        if (op_char == '1'){
+            
+        }
+        else if(op_char == '2'){
+            thread_unmount(session_id);
+        }
+        else if(op_char == '3'){
+            thread_open(session_id);
+        }
+        else if(op_char == '4'){
+            thread_close(session_id);
+        }
+        else if(op_char == '5'){
+            thread_write(session_id);
+        }
+        else if(op_char == '6'){
+            thread_read(session_id);
+        }
+        else if(op_char == '7'){
+            thread_destroy_after_all_closed(session_id);
+        }
+    }
+}
+
 
 int main(int argc, char **argv) {
 
@@ -167,60 +179,104 @@ int main(int argc, char **argv) {
         }
     }
     if(tfs_init() == -1) return -1;
-    for (int i; i < MAX_SESSIONS; i++){
-        pthread_create(&session_list[i].thread, NULL, &write, NULL);
+    pthread_mutex_init(&server_thread_lock, NULL);
+    for (int i = 0; i < MAX_SESSIONS; i++){
+        pthread_mutex_lock(&server_thread_lock);
         pthread_cond_init(&session_list[i].var, NULL);
+        pthread_mutex_init(&session_list[i].lock, NULL);
+        pthread_create(&session_list[i].thread, NULL, &thread_func, (void*)&i);
+        printf(" %d\t created\n", i);
     }
 
     server_pipe = open(pipename, O_RDONLY);
-//    printf("\t server pipe opened\n");
 
-    int a;
+    printf("\t thread open\n");
+    ssize_t a;
     while(1){
         char op_code;
         a = read(server_pipe, &op_code, sizeof(char));
         if (a > 0){
             if(op_code == '1'){
                 
-                thread_mount();
+                thread_mount();     //comentar o porque de nao dar thread nisto
 
             } else if(op_code == '2'){
-                
                 printf("TFS_UNMOUNT:\n");
-                
+                                
                 unmount_struct input;
                 if(read(server_pipe, &input, sizeof(unmount_struct)) == -1){
                     printf("ERROR: reading client name\n");
                 }
-                                
-                memcpy(session_list[input.session_id].buffer, &input, sizeof(unmount_struct));
+                
                 session_list[input.session_id].op_code = '2';
 
                 pthread_cond_signal(&session_list[input.session_id].var);
-                
-                //thread_func();
-                
-                //thread_unmount();
             
             } else if(op_code == '3'){
+                printf("TFS_OPEN:\n");
 
-                thread_open();
+                open_struct input;
+                if(-1 == read(server_pipe, &input, sizeof(open_struct))) return -1;
+                //printf("\t read from server pipe\n");
+                session_list[input.session_id].buffer = malloc(sizeof(open_struct));
+                memcpy(session_list[input.session_id].buffer, &input, sizeof(open_struct));
+                session_list[input.session_id].op_code = '3';
+
+                pthread_cond_signal(&session_list[input.session_id].var);
+                printf("\t session id: %d\n", input.session_id);
             
             } else if(op_code == '4'){
+                printf("TFS_CLOSE:\n");
 
-                thread_close();
+                close_struct input;
+                if(-1 == read(server_pipe, &input, sizeof(close_struct))) return -1;
+                
+                session_list[input.session_id].buffer = malloc(sizeof(close_struct));
+                memcpy(session_list[input.session_id].buffer, &input, sizeof(close_struct));
+                session_list[input.session_id].op_code = '4';
+
+                pthread_cond_signal(&session_list[input.session_id].var);
             
             } else if(op_code == '5'){
+                printf("TFS_WRITE:\n");
 
-                thread_write();
+
+                write_struct input;
+                if(-1 == read(server_pipe, &input, sizeof(write_struct))) return -1;
+                char buffer[input.len /* + 1*/ ];
+                //printf("\t created buffer \"%d\"\n", &buffer);
+               if(-1 == read(server_pipe, &buffer, input.len)) return -1;
+                //buffer[input.len] = '\0';
+                printf("\t read from server pipe: %s", buffer);
+                session_list[input.session_id].buffer = malloc(sizeof(write_struct) + input.len);
+                memcpy(session_list[input.session_id].buffer, &input, sizeof(write_struct));
+                memcpy(session_list[input.session_id].buffer + sizeof(write_struct), &buffer, input.len);
+                session_list[input.session_id].op_code = '5';
+
+                pthread_cond_signal(&session_list[input.session_id].var);
+                printf("\t session id: %d\n", input.session_id);
             
             } else if(op_code == '6'){
 
-                thread_read();
+                printf("TFS_READ:\n");
+                read_struct input;
+                if(-1 == read(server_pipe, &input, sizeof(read_struct))) return -1;
+                
+                session_list[input.session_id].buffer = malloc(sizeof(read_struct));
+                memcpy(session_list[input.session_id].buffer, &input, sizeof(read_struct));
+                session_list[input.session_id].op_code = '6';
+
+                pthread_cond_signal(&session_list[input.session_id].var);
             
             }else if(op_code == '7'){
 
-                thread_destroy_after_all_closed();
+                printf("TFS_DESTROY_AFTER_ALL_CLOSED:\n");
+                int session_id;
+                if(-1 == read(server_pipe, &session_id, sizeof(int))) return -1;
+
+                session_list[session_id].op_code = '7';
+
+                pthread_cond_signal(&session_list[session_id].var);
             }
         }
     }
